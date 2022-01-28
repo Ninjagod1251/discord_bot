68bf6300b9dfeade413200f403b7f2fa346dd481
15219900+Ninjagod1251@users.noreply.github.com <15219900+Ninjagod1251@users.noreply.github.com>
2022-01-28 13:14:37 -0800

    feat: first commit

---
+++ main.py
@@ -0,0 +1,72 @@
+from http import client
+
+import discord
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+APE_GUILD = os.getenv('APE_GUILD')
+ETH_GUILD = os.getenv('ETH_GUILD')
+CAO_GUILD = os.getenv('CAO_GUILD')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'
+    .format(client))
+
+    guild = discord.utils.get(client.guilds, name=APE_GUILD)
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+def list_gen(guild):
+    name_list = []
+    for channel in guild:
+        name_list.append(channel.name)
+    return name_list
+
+
+async def echo(message, ape_guild, listening_guild):
+    # send msg to APE from origin
+    if message.guild.name == CAO_GUILD:
+        name_list = list_gen(ape_guild.text_channels)
+        new_message = f"**Server** {message.guild} **Channel** {message.channel.name} " \
+                      f"**Author** {message.author.display_name}\n " + message.content
+        index_of_name_list = name_list.index('testing')
+
+        await ape_guild.text_channels[index_of_name_list].send(new_message)
+        await message.channel.send('Your bug has been sent to ApeWorx/Bug Channel')
+
+    # send msg from ape to origin
+    if message.guild.name == APE_GUILD:
+        new_message = f"**Server** {message.guild} **Channel** {message.channel.name} " \
+                      f"**Author** {message.author.display_name}\n " + message.content
+        name_list = list_gen(listening_guild.text_channels)
+        index_of_name_list = name_list.index('general')
+        await listening_guild.text_channels[index_of_name_list].send(new_message)
+        await message.channel.send('Your bug has been sent to ApeWorx/Bug Channel')
+
+
+@client.event
+async def on_message(message):
+    ape_guild = discord.utils.get(client.guilds, name=APE_GUILD)
+    listening_guild = discord.utils.get(client.guilds, name=CAO_GUILD)
+    if message.author == client.user:
+        return
+    # echos a bug to the targeted channel
+    if message.content.startswith('$bug'):
+        await echo(message, ape_guild, listening_guild)
+
+
+
+
+    # says hello to the user
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello')
+
+client.run(TOKEN)
